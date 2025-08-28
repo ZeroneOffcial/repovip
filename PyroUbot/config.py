8926a0eeda0a6877eb553398da0ae383a9c868dd
ZeroneOffcial <rafliwidyanata879@gmail.com>
2025-08-29 00:54:23 +0700

    Update config.py

--- PyroUbot/config.py
+++ PyroUbot/config.py
@@ -5,22 +5,22 @@ load_dotenv(".env")
 
 MAX_BOT = int(os.getenv("MAX_BOT", "100"))
 
-DEVS = list(map(int, os.getenv("DEVS", "5850202522").split()))
+DEVS = list(map(int, os.getenv("DEVS", "7716690774").split()))
 
-API_ID = int(os.getenv("API_ID", "23117267"))
+API_ID = int(os.getenv("API_ID", "27095838"))
 
-API_HASH = os.getenv("API_HASH", "0e3230b3f1f8c32d66b95fa1853a5b26")
+API_HASH = os.getenv("API_HASH", "28c51c248482f0ca68d091397e2e491b")
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8076346528:AAGmUdPs-Ip7OXSkqQFZumNAKAS0Ug0cRjQ")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8085087229:AAFcA1ntR6fUez1j9fb2zRAYPmMxQ2uxUpw")
 
-OWNER_ID = int(os.getenv("OWNER_ID", "5850202522"))
+OWNER_ID = int(os.getenv("OWNER_ID", "7716690774"))
 
 BLACKLIST_CHAT = list(map(int, os.getenv("BLACKLIST_CHAT", "-1002125842026 -1002053287763 -1002044997044 -1002022625433 -1002050846285 -1002400165299 -1002416419679 -1001473548283").split()))
 
 RMBG_API = os.getenv("RMBG_API", "ybwGCgqJngeYGQLiWAjxrYoz")
 
-MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://ndyuserbot:ndyuserbot@cluster0.wvdpaip.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://ZeroneOffcial:ZeroneOffcial@cluster0.3ypqmpw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
-LOGS_MAKER_UBOT = os.getenv("LOGS_MAKER_UBOT", "-1002791340508")
+LOGS_MAKER_UBOT = os.getenv("LOGS_MAKER_UBOT", "-1002866809765")
 
-USER_GROUP = os.getenv("USER_GROUP", "@INFORMATIONNDY")
+USER_GROUP = os.getenv("USER_GROUP", "@channelszerone")
