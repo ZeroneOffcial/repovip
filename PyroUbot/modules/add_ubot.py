fafd44e3c7b646340d7eee6dcab03c4effc4dd6c
ZeroneOffcial <rafliwidyanata879@gmail.com>
2025-08-29 16:34:48 +0700

    Update add_ubot.py

--- PyroUbot/modules/add_ubot.py
+++ PyroUbot/modules/add_ubot.py
@@ -18,7 +18,7 @@ async def _(client, message):
     msg = MSG.START(message)
 
     await message.reply_photo(
-        photo="https://files.catbox.moe/109k7i.jpg", 
+        photo="https://files.catbox.moe/ep6iv4.jpg", 
         caption=msg, 
         reply_markup=InlineKeyboardMarkup(buttons)
     )
