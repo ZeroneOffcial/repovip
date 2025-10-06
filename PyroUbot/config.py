a0ef38cec4f2db8457d5d1800088eb40b290722e
ZeroneOffcial <rafliwidyanata879@gmail.com>
2025-10-06 23:07:21 +0700

    Update config.py

--- PyroUbot/config.py
+++ PyroUbot/config.py
@@ -5,7 +5,7 @@ load_dotenv(".env")
 
 MAX_BOT = int(os.getenv("MAX_BOT", "100"))
 
-DEVS = list(map(int, os.getenv("DEVS", "7716690774", "6511631892").split()))
+DEVS = list(map(int, os.getenv("DEVS", "6511631892").split()))
 
 API_ID = int(os.getenv("API_ID", "27095838"))
 
@@ -13,7 +13,7 @@ API_HASH = os.getenv("API_HASH", "28c51c248482f0ca68d091397e2e491b")
 
 BOT_TOKEN = os.getenv("BOT_TOKEN", "8367633363:AAES46kXP707cECaTNJjWC3pAgoHsoWvjf8")
 
-OWNER_ID = int(os.getenv("OWNER_ID", "7716690774", "6511631892"))
+OWNER_ID = int(os.getenv("OWNER_ID", "7716690774"))
 
 BLACKLIST_CHAT = list(map(int, os.getenv("BLACKLIST_CHAT", "-1002125842026 -1002053287763 -1002044997044 -1002022625433 -1002050846285 -1002400165299 -1002416419679 -1001473548283").split()))
 
